e4eec27f38d197da372a87ef3cb09bdc54e68ee7
fsksf <timeless.go@foxmail.com>
2021-02-02 19:20:21 +0800

    feat: tushare

--- tinigine/mod/data_from_tushare/__init__.py
+++ tinigine/mod/data_from_tushare/__init__.py
@@ -1,4 +1,9 @@
 
+from tinigine.utils.config import ConfigManager
+
+mod_conf = ConfigManager(mod_name='data_from_tushare')
+
+
 def load():
     from .mod import DataFromTushare
     mod_instance = DataFromTushare()
