af6d685d5603fe7f9388c0dc8d4dbbc419bb694b
fsksf <timeless.go@foxmail.com>
2021-02-05 18:30:15 +0800

    init basic

--- tinigine/utils/db.py
+++ tinigine/utils/db.py
@@ -15,3 +15,15 @@ SQLALCHEMY_DATABASE_URI = conf.get_config()['db']['main']
 db = create_engine(SQLALCHEMY_DATABASE_URI)
 DBSession = sessionmaker(bind=db)
 Base = declarative_base()
+
+
+class DBConnect:
+
+    def __init__(self):
+        self._s = DBSession()
+
+    def __enter__(self):
+        return self._s
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._s.close()
