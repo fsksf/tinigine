9416f6f431333620345444001de4c3874361227e
fsksf <timeless.go@foxmail.com>
2021-02-12 09:39:09 +0800

    add readme

--- tinigine/mod/data_from_tushare/data_proxy.py
+++ tinigine/mod/data_from_tushare/data_proxy.py
@@ -66,7 +66,7 @@ class MysqlDataProxy(AbstractDataProxy, ABC):
         """
         symbols = self.download_symbols()
         start, end = self.download_calender()
-        self.download_quote(symbols, 20100104, 20211231)
+        self.download_quote(symbols, start, end)
 
     def download_symbols(self):
         new_basic = DataUtilFromTushare.load_basic(self._env.params.market)
