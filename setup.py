04c6033806759545fe480afdfd1425164833615d
fsksf <timeless.go@foxmail.com>
2021-02-12 09:37:56 +0800

    add readme

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""
+@project: tinigine
+@author: kang 
+@github: https://github.com/fsksf 
+@since: 2021/2/12 8:17 AM
+"""
+
+import os
+import codecs
+from distutils.core import setup
+
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='tinigine',
+    version=read('tinigine/VERSION.txt'),
+    packages=['tinigine', 'tinigine.core', 'tinigine.utils', 'tinigine.samples'],
+    author='fsksf',
+    author_email='timeless.go@foxmail.com',
+    description='stock local data',
+    package_data={'': ['*.*']},
+    include_package_data=True,
+    url='https://github.com/fsksf/ginkgo',
+    entry_points={
+        "console_scripts": [
+            "tinigine = tinigine.__main__:entry"
+        ]
+    },
+    install_requires=read('requirements.txt')
+)
