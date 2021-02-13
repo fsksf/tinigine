6ca88cf11e06c4162df29c0ebe12fe69e4fad443
fsksf <timeless.go@foxmail.com>
2021-02-13 08:13:48 +0800

    fix history

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -25,12 +25,12 @@ def initialize(context):
 def on_bar(context, data):
     print(context)
     print(data.current())
-    print(data.history(5))
+    print(data.history(5).to_dataframe())
 
 """
 
 
-class TestStrategy:
+class TestEngine:
 
     def setup(self):
         params = Params(algo_text=strategy_str, algo_file=None, capital=10000000, start=20190102, end=20191101, freq='1d',
