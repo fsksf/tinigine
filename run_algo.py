692832eb6b4f8bbfa3ba07eb346fa15db823f9f1
fsksf <timeless.go@foxmail.com>
2021-03-01 20:57:28 +0800

    fix: progress error

--- run_algo.py
+++ run_algo.py
@@ -4,3 +4,35 @@
 
 @since: 2020-04-25 21:21
 """
+
+from tinigine.core.engine import Engine
+from tinigine.core.env import Environment
+from tinigine.core.params import Params
+from tinigine.config import conf
+
+strategy_str = """
+from tinigine.api import *
+
+def initialize(context):
+    print(context)
+    subscribe('000001.SZ', 10)
+
+def on_bar(context, data):
+    print(context)
+    print(data.current())
+    print(data.history(5).to_dataframe())
+
+"""
+
+
+def main():
+    p_config = conf.get_config()['params']
+    params = Params(**p_config)
+    params.set_algo(strategy_str)
+    env = Environment(params)
+    engine = Engine(env)
+    engine.run()
+
+
+if __name__ == '__main__':
+    main()
