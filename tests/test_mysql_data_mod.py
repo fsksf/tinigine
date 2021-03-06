4ab1fceba3522b02ab3f413e0b737572ebc7a4f9
fsksf <timeless.go@foxmail.com>
2021-03-06 11:00:00 +0800

    add unittest

--- tests/test_mysql_data_mod.py
+++ tests/test_mysql_data_mod.py
@@ -21,9 +21,8 @@ def initialize(context):
     subscribe(['000001.SZ'], 10)
 
 def on_bar(context, data):
-    print(context)
     print(data.current())
-    print(data.history(5).to_dataframe())
+
 
 """
 
@@ -39,10 +38,37 @@ class TestEngine:
         event_source = EventSource(environment)
         environment.set_event_source(event_source)
         engine = Engine(environment)
+        self._env = environment
         self._engine = engine
+        self._env.data_proxy.subscribe(['000001.SZ', '600600.SH'], before_bar_count=10)
 
     def teardown(self):
         pass
 
     def test_events(self):
-        self._engine.run()
+        # self._engine.run()
+        pass
+
+    def test_symbols(self):
+        symbols = self._env.data_proxy.get_symbols()
+        assert symbols == ['000001.SZ', '600600.SH', '600703.SH']
+
+    @pytest.mark.parametrize("start,end,result", [
+        (20100101, 20200101, 20100104),
+        (20180101, 20200101, 20180102)
+    ])
+    def test_calender(self, start, end, result):
+
+        calender = self._env.data_proxy.get_calendar(start, end)
+        assert calender[0] == result
+
+    def test_subscribe(self):
+        first_data = self._env.data_proxy.data_walker.current()
+        assert first_data['close']['000001.SZ'] == 9.19
+
+    def test_data_walker(self):
+        data_walker = self._env.data_proxy.data_walker
+        current = data_walker.current()
+        assert current['open']['000001.SZ'] == 9.39
+        history = data_walker.history(3)
+        print(history.to_dataframe())
